eb2b767547463552177a32f29ab4e0ed0090636f
Rajeev Roy <109815313+RajeevRoy120@users.noreply.github.com>
2025-03-19 17:39:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function functionName() {
+    // ✅ Display the function's name using arguments.callee.name
+    alert(arguments.callee.name);
+}
+
+// ✅ Call the function to test
+functionName();
